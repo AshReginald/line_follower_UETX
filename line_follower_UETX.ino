89f12f20ce191519c80b432f257edfd847b8f4b1
AshReginald <141470836+AshReginald@users.noreply.github.com>
2023-10-27 09:24:23 +0700

    line_follower_UETX.ino

--- line_follower_UETX.ino
+++ line_follower_UETX.ino
@@ -1,9 +1,25 @@
-#include <AFMotor.h>
 
-AF_DCMotor motor1(1); // Khai báo động cơ 1
-AF_DCMotor motor2(2); // Khai báo động cơ 2
+#include <QTRSensors.h>
 
-int sensorPins[8] = {A0, A1, A2, A3, A4, A5, A6, A7}; // Đầu dò line
+QTRSensors qtr;
+// Khai báo chân cho module L298N cho động cơ 1
+int ENA1 = 10;   // Chân enable động cơ 1
+int IN1 = 8;
+int IN2 = 9;
+
+// Khai báo chân cho module L298N cho động cơ 2
+int ENA2 = 11;   // Chân enable động cơ 2
+int IN3 = 12;
+int IN4 = 13;
+
+float output=0;
+float   P = 0, I = 0, D=0;
+float preErr = 0;
+int left_pwm, right_pwm;
+int Err;
+
+#define LEFT_SPEED 120
+#define RIGHT_SPEED 160
 
 // Các hằng số PID
 float kp = 1.5;   // Hệ số tỷ lệ
@@ -16,73 +32,121 @@ float currentPosition = 0;   // Vị trí hiện tại
 float prevError = 0;         // Sai số trước đó
 float integral = 0;          // Tích phân
 
-void setup() {
+// Khai báo chân cho encoder động cơ 1
+int encoderPinA1 = 2;  // Kết nối với dây C1 của encoder động cơ 1
+int encoderPinB1 = 3;  // Kết nối với dây C2 của encoder động cơ 1
+
+// Khai báo chân cho encoder động cơ 2
+int encoderPinA2 = 4;  // Kết nối với dây C1 của encoder động cơ 2
+int encoderPinB2 = 7;  // Kết nối với dây C2 của encoder động cơ 2
+
+volatile long encoderCount1 = 0;  // Số xung từ encoder động cơ 1
+volatile long encoderCount2 = 0;  // Số xung từ encoder động cơ 2
+
+
+const uint8_t SensorCount = 8;
+uint16_t sensorValues[SensorCount];
+
+void setup()
+{
+  // configure the sensors
+  qtr.setTypeAnalog();
+  qtr.setSensorPins((const uint8_t[]){5, 6, A5, A4, A3, A2, A1, A0}, SensorCount);
+  qtr.setEmitterPin(2);
+  
+
   Serial.begin(9600);
-  for (int i = 0; i < 8; i++) {
-    pinMode(sensorPins[i], INPUT);
+  pinMode(ENA1, OUTPUT);
+  pinMode(IN1, OUTPUT);
+  pinMode(IN2, OUTPUT);
+  pinMode(ENA2, OUTPUT);
+  pinMode(IN3, OUTPUT);
+  pinMode(IN4, OUTPUT);
+
+  pinMode(encoderPinA1, INPUT);
+  pinMode(encoderPinB1, INPUT);
+  pinMode(encoderPinA2, INPUT);
+  pinMode(encoderPinB2, INPUT);
+
+  attachInterrupt(digitalPinToInterrupt(encoderPinA1), updateEncoder1, CHANGE);
+  attachInterrupt(digitalPinToInterrupt(encoderPinA2), updateEncoder2, CHANGE);
+}
+void FORWARD(){
+  analogWrite(IN1,200);
+  analogWrite(IN2,0);
+  analogWrite(IN3,0);
+  analogWrite(IN4,200);
+  analogWrite(ENA1, 200);
+  analogWrite(ENA2, 200);
+}
+void LEFT(){
+  analogWrite(IN1,0);
+  analogWrite(IN2,0);
+  analogWrite(IN3,200);
+  analogWrite(IN4,0);
+}
+void RIGHT (){
+  analogWrite(IN1,200);
+  analogWrite(IN2,0);
+  analogWrite(IN3,0);
+  analogWrite(IN4,0);
+}
+void STOP(){
+  analogWrite(IN1,0);
+  analogWrite(IN2,0);
+  analogWrite(IN3,0);
+  analogWrite(IN4,0);
+}
+void loop()
+{
+  int pin[9];
+  // read raw sensor values
+  qtr.read(sensorValues);
+
+  // print the sensor values as numbers from 0 to 1023, where 0 means maximum
+  // reflectance and 1023 means minimum reflectance
+  for (uint8_t i = 0; i < SensorCount; i++)
+  {
+    Serial.print(sensorValues[i]);
+    pin[i] = sensorValues[i];
+    Serial.print('\t');
+  }
+  Serial.println();
+
+  delay(250);
+  if(pin[0] > 900 && pin[1] > 900 && pin[2] > 900 && pin[3] > 900 && pin[4] > 900 && pin[5] > 900 && pin[6] > 900 && pin[7] > 900) {
+     FORWARD();
+  }
+  else if(pin[0] < 900 && pin[1] < 900) {
+     RIGHT();
+  }
+  else if(pin[6] < 900 && pin[7] < 900) {
+     LEFT();
+  }
+  analogWrite(ENA1, 200);
+  analogWrite(ENA2, 200);
+  // Đọc giá trị encoder cho động cơ 1 và động cơ 2
+  long count1 = encoderCount1;
+  long count2 = encoderCount2;
+
+
+  // Thực hiện xử lý dữ liệu encoder ở đây
+
+  delay(100);  // Đọc giá trị encoder mỗi 100ms
+}
+
+void updateEncoder1() {
+  if (digitalRead(encoderPinB1) == LOW) {
+    encoderCount1++;
+  } else {
+    encoderCount1--;
   }
 }
 
-void loop() {
-  int sensorValues[8];
-  
-  // Đọc giá trị từ các đầu dò
-  for (int i = 0; i < 8; i++) {
-    sensorValues[i] = digitalRead(sensorPins[i]);
-  }
-  
-  // Kiểm tra vị trí của dòng line
-  int leftWeight = 0, rightWeight = 0;
-  for (int i = 0; i < 8; i++) {
-    if (sensorValues[i] == LOW) {
-      leftWeight += (i + 1);
-      rightWeight += (8 - i);
-    }
-  }
-  
-  // Tính giá trị sai số
-  float error = leftWeight - rightWeight;
-  
-  // Tính thành phần PID
-  integral += error;
-  float derivative = error - prevError;
-  
-  // Tính động cơ điều khiển từ PID
-  float motorControl = kp * error + ki * integral + kd * derivative;
-  
-  // Giới hạn giá trị động cơ điều khiển
-  motorControl = constrain(motorControl, -255, 255);
-  
-  // Tính tốc độ motor từ động cơ điều khiển
-  int motorSpeed1 = 255 - (int)motorControl;
-  int motorSpeed2 = 255 + (int)motorControl;
-  
-  // Tăng tốc độ lên tối đa khi xe đi thẳng
-  if (error == 0) {
-    motorSpeed1 = 255;
-    motorSpeed2 = 255;
-  }
-  
-  // Giới hạn tốc độ motor
-  motorSpeed1 = constrain(motorSpeed1, 0, 255);
-  motorSpeed2 = constrain(motorSpeed2, 0, 255);
-  
-  // Điều khiển động cơ
-  motor1.setSpeed(motorSpeed1);
-  motor2.setSpeed(motorSpeed2);
-  
-  // Đảo chiều động cơ dựa trên trọng số
-  if (leftWeight > rightWeight) {
-    motor1.run(BACKWARD);
-    motor2.run(FORWARD);
-  } else if (rightWeight > leftWeight) {
-    motor1.run(FORWARD);
-    motor2.run(BACKWARD);
+void updateEncoder2() {
+  if (digitalRead(encoderPinB2) == LOW) {
+    encoderCount2++;
   } else {
-    motor1.run(FORWARD);
-    motor2.run(FORWARD);
+    encoderCount2--;
   }
-  
-  // Lưu giá trị sai số trước đó
-  prevError = error;
 }
